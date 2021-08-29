ef715b604d5b0a5d3a83f5a4a669e67810bedc9c
donggune kim <kdg96g@gmail.com>
2021-08-29 13:24:49 +0900

    categoryItem 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import GlobalStyle from 'components/Common/GlobalStyle';
+import Footer from 'components/Common/Footer';
+import CategoryList from 'components/Main/CategoryList';
 import Introduction from 'components/Main/Introduction';
+
+const CATEGORY_LIST = {
+    All: 5,
+    Web: 3,
+    Mobile: 2,
+};
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
 `;
 
 const IndexPage: FunctionComponent = function () {
@@ -14,6 +22,8 @@ const IndexPage: FunctionComponent = function () {
         <Container>
             <GlobalStyle />
             <Introduction />
+            <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
+            <Footer />
         </Container>
     );
 };
